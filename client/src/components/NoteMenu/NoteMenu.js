5c0b72d61d0c339d0d1507a7ba4fcb386e0b3819
Brennan Wilkins <bwilkins767@gmail.com>
2021-03-26 16:56:48 -0500

    Added note menu icons

--- client/src/components/NoteMenu/NoteMenu.js
+++ client/src/components/NoteMenu/NoteMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './NoteMenu.css';
 import PropTypes from 'prop-types';
-import { expandIcon, contractIcon } from '../UI/icons';
+import { expandIcon, contractIcon, trashIcon, shareIcon, pinIcon } from '../UI/icons';
 import { connect } from 'react-redux';
 import { toggleFullscreen } from '../../store/actions';
 import Tooltip from '../UI/Tooltip/Tooltip';
@@ -10,10 +10,24 @@ const NoteMenu = props => {
   return (
     <div className="NoteMenu">
       <div className="NoteMenu__btns">
-        <button className="NoteMenu__fsBtn" onClick={props.toggleFullscreen}>
+        <button className="NoteMenu__btn" onClick={props.toggleFullscreen}>
           {props.isFullscreen ? contractIcon : expandIcon}
           <Tooltip position="down">Fullscreen<div>Ctrl+Shift+F</div></Tooltip>
         </button>
+        <div className="NoteMenu__options">
+          <button className="NoteMenu__optionBtn">
+            {pinIcon}
+            <Tooltip position="down">Pin to top</Tooltip>
+          </button>
+          <button className="NoteMenu__optionBtn">
+            {shareIcon}
+            <Tooltip position="down">Share</Tooltip>
+          </button>
+          <button className="NoteMenu__optionBtn">
+            {trashIcon}
+            <Tooltip position="down">Send to trash</Tooltip>
+          </button>
+        </div>
       </div>
     </div>
   );

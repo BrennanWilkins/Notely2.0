c1d72a53a3a6c78c2cde990fc91bb39e67563be0
Brennan Wilkins <bwilkins767@gmail.com>
2021-04-02 16:23:28 -0500

    Added note preview spinner/err msg

--- client/src/components/NotePreview/NotePreview.js
+++ client/src/components/NotePreview/NotePreview.js
@@ -5,18 +5,32 @@ import './NotePreview.css';
 import PropTypes from 'prop-types';
 import { CloseBtn } from '../UI/Buttons/Buttons';
 import { sendUpdate } from '../../socket';
+import SimpleSpinner from '../UI/SimpleSpinner/SimpleSpinner';
 
 const NotePreview = props => {
   const [value, setValue] = useState([]);
   const editor = useMemo(() => withReact(createEditor()), []);
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState(false);
 
   useEffect(() => {
+    setIsLoading(false);
     const socket = sendUpdate('get/note/invite', { noteID: props.noteID });
 
     socket.on('success: get/note/invite', data => {
       socket.off('success: get/note/invite');
+      socket.off('error: get/note/invite');
       const { body } = JSON.parse(data);
       setValue(body);
+      setIsLoading(false);
+      setErr(false);
+    });
+
+    socket.on('error: get/note/invite', msg => {
+      socket.off('success: get/note/invite');
+      socket.off('error: get/note/invite');
+      setErr(true);
+      setIsLoading(false);
     });
   }, [props.noteID]);
 
@@ -24,9 +38,18 @@ const NotePreview = props => {
     <div className="NotePreview">
       <CloseBtn className="NotePreview__closeBtn" onClick={props.close} />
       <div className="NotePreview__slate">
-        <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-          <Editable readOnly placeholder="Empty Note" />
-        </Slate>
+        {
+          err ?
+            <div className="NotePreview__errMsg">
+              There was an error while retrieving the note preview.
+            </div>
+          : isLoading ?
+            <SimpleSpinner className="NotePreview__spinner" />
+          :
+            <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+              <Editable readOnly placeholder="Empty Note" />
+            </Slate>
+        }
       </div>
     </div>
   );
---
+++ client/src/components/UI/SimpleSpinner/SimpleSpinner.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './SimpleSpinner.css';
+
+const SimpleSpinner = props => <div className={`SimpleSpinner ${props.className || ''}`} />;
+
+export default SimpleSpinner;

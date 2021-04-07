5e4adf0c245870ef7026c5ffa3fed3cf2b1ffd36
Brennan Wilkins <bwilkins767@gmail.com>
2021-04-07 16:20:19 -0500

    op type check

--- client/src/components/NoteContent/NoteContent.js
+++ client/src/components/NoteContent/NoteContent.js
@@ -30,6 +30,17 @@ const BLOCK_HOTKEYS = {
   'mod+shift+6': 'check-list-item'
 };
 
+const OP_TYPES = {
+  'insert_text': true,
+  'remove_text': true,
+  'insert_node': true,
+  'merge_node': true,
+  'move_node': true,
+  'remove_node': true,
+  'set_node': true,
+  'split_node': true
+};
+
 const NoteContent = props => {
   const [value, setValue] = useState(props.currentBody);
   const renderElement = useCallback(props => <Element {...props} />, []);
@@ -53,6 +64,7 @@ const NoteContent = props => {
       isRemoteChange.current = true;
       Editor.withoutNormalizing(editor, () => {
         data.ops.forEach(op => {
+          if (!OP_TYPES[op.type]) { return; }
           editor.apply(op);
         });
       });

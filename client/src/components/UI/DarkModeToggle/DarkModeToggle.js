b290e6648fec2f958023e8c16ece2a339476e53b
Brennan Wilkins <bwilkins767@gmail.com>
2021-04-18 00:27:28 -0500

    Updated dark mode toggle

--- client/src/components/UI/DarkModeToggle/DarkModeToggle.js
+++ client/src/components/UI/DarkModeToggle/DarkModeToggle.js
@@ -5,27 +5,20 @@ import { sunIcon, moonIcon } from '../icons';
 import { connect } from 'react-redux';
 import { toggleDarkMode } from '../../../store/actions';
 
-const DarkModeToggle = props => {
-  return (
-    <div className={`DarkModeToggle ${props.darkMode ? 'DarkModeToggle--dark' : 'DarkModeToggle--light'}`}>
-      <div className="DarkModeToggle__sun">{sunIcon}</div>
-      <div className="DarkModeToggle__moon">{moonIcon}</div>
-      <div className="DarkModeToggle__btn" onClick={props.toggleDarkMode} />
-    </div>
-  );
-};
+const DarkModeToggle = props => (
+  <div className="DarkModeToggle" onClick={props.toggleDarkMode}>
+    <div className="DarkModeToggle__sun">{sunIcon}</div>
+    <div className="DarkModeToggle__moon">{moonIcon}</div>
+    <div className="DarkModeToggle__btn" />
+  </div>
+);
 
 DarkModeToggle.propTypes = {
-  darkMode: PropTypes.bool.isRequired,
   toggleDarkMode: PropTypes.func.isRequired
 };
-
-const mapStateToProps = state => ({
-  darkMode: state.ui.darkMode
-});
 
 const mapDispatchToProps = dispatch => ({
   toggleDarkMode: () => dispatch(toggleDarkMode())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(DarkModeToggle);
+export default connect(null, mapDispatchToProps)(DarkModeToggle);
